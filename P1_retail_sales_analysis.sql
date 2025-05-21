d08d36d3a5f21410da1c861d53d9db3e1c0d1313
Vivek Ranjan <vivekranjan21639@gmail.com>
2025-05-21 16:58:32 +0530

    initial commit

--- P1_retail_sales_analysis.sql
+++ P1_retail_sales_analysis.sql
@@ -45,12 +45,12 @@ where transactions_id is null or
 	total_sale is null;
 
 
--- remove the data point where qunatity was null
+-- As data suggested there are 3 null entries in the quantity column. Since the no. of entries are not large we can 
+-- **remove these** rows for the purpose of data cleaning.
 
 delete from retail_sales where quantity is null;
 
--- checking  how many data points are there for each pair of uniques values
--- for categiry and gender. 
+--  
 select category, 
 		count(case when gender = 'Male' then 1 end) as Male_count,
 		count(case when gender = 'Female' then 1 end) as Female_count
@@ -199,13 +199,141 @@ from retail_sales group by category, gender order by category, rw
 select category, gender, no_of_orders from cte where rw = 1;
 
 -- Customer age groups who placed highest no. of sales?
+
+select age_category, sum(total_sale) as net_sales
+from retail_sales
+group by age_category
+order by net_sales desc;
+
 -- calculate total sales each month in each year.
+select 
+	extract(year from sale_date) as sale_year,
+	extract(month from sale_date) as sale_month,
+	count(total_sale) as net_sale
+from retail_sales
+group by sale_year, sale_month
+order by sale_year, sale_month;
+
 -- Find out number of unique customers who purchased at least one product from each category?
--- which is the highest revenue generating category? 
+with cte as (
+	select customer_id
+	from retail_sales
+	group by customer_id
+	having count(distinct category) = (select count(distinct category) from retail_sales)
+)
+select count(distinct customer_id) from cte;
+
+-- which is the highest revenue generating category?
+select 
+	category, 
+	sum(total_sale) as revenue 
+from retail_sales
+group by category
+order by revenue desc
+limit 1;
+
+select * from retail_sales;
 -- find out the customers who placed orders higher than the average order value for each category?
+
+WITH cat_avg as(
+	select category, round(avg(total_sale):: numeric, 2) as avg_by_cat
+	from retail_sales group by category
+),
+cust_avg as(
+	select customer_id, category, round(avg(total_sale):: numeric, 2) as avg_by_cust
+	from retail_sales group by customer_id, category
+)
+select customer_id, cust_avg.category, avg_by_cust, avg_by_cat 
+from cat_avg right join cust_avg on cat_avg.category = cust_avg.category
+where avg_by_cust > avg_by_cat
+order by customer_id;
+
 -- find out the top 20% of the customer in terms of  sales value.
--- Find all the transaction where the total  sale is greater than 1000.
+with cust_tab as (
+	select customer_id, sum(total_sale) as total_spending
+	from retail_sales
+	group by customer_id
+),
+tiled_cust as(
+	select *, Ntile(5) over (order by total_spending desc) as bucket_cat from cust_tab
+)
+select * from tiled_cust where bucket_cat = 1;
+
+
+with cust_tab as (
+	select customer_id, sum(total_sale) as total_spending
+	from retail_sales
+	group by customer_id
+),
+ranked_cust as(
+	select *, percent_rank() over(order by total_spending desc) as rnk
+	from cust_tab
+)
+select customer_id, total_spending from ranked_cust where rnk < 0.2;
+
+-- Find all the transaction where the total sale is greater than 1000.
+select * 
+from retail_sales
+where total_sale > 1000;
+
 -- find out any seasonality if exist for each product.
--- find out the time interval iin which the most of the (80%) of the orders are being placed.
--- Retrieve all columns for sales made on '2022-11-05'.
--- Retriev all transactions where the category is clothing and the quantity sold id more than 10in the month of Nov-2022.
+with cte1 as(
+	select
+		category,
+		extract(year from sale_date) as sale_yr, 
+		extract(month from sale_date) as sale_month, 
+		count(transactions_id) as no_of_transc
+	from retail_sales
+	group by category, sale_yr, sale_month
+),
+cte2 as (
+	select *, 
+	row_number() over(partition by category, sale_yr order by no_of_transc desc) as rn
+	from cte1
+)
+select * 
+from cte2
+where rn in (1,2,3,4);
+order by category, sale_yr;
+
+-- find out the time interval in which the most of the of the orders are being placed.
+with cte as(
+select
+	case when sale_time between '06:00:00' and '10:00:00' then 'Morning'
+			when sale_time between '10:00:00' and '13:00:00' then 'Noon'
+			when sale_time between '13:00:00' and '17:00:00' then 'evening'
+			when sale_time between '17:00:00' and '20:00:00' then 'late evening'
+			when sale_time between '20:00:00' and '23:00:00' then 'night'
+			else 'late night'
+			end as time_segment,
+	count(transactions_id) as no_of_orders
+from retail_sales
+group by time_segment
+order by no_of_orders desc
+),
+ranked as(
+	select *, 
+	sum(no_of_orders) over(order by no_of_orders desc) as cummulative_orders,
+	sum(no_of_orders) over() as total_orders
+	from cte
+),
+fin as(
+select *, 
+round((cummulative_orders*100)/total_orders,2) as cummulative_percent 
+from ranked
+)
+select * 
+from fin
+where cummulative_percent <= 80;
+
+-- Retrieve all columns for sales made on '2022-11-05'
+select *
+from retail_sales
+where sale_date = '2022-11-05';
+
+-- Retriev all transactions where the category is clothing and the quantity sold is more than 3 in the month of Nov-2022.
+select transactions_id, quantity, category, sale_date 
+from retail_sales
+where category = 'Clothing' and
+	quantity > 3 and 
+	to_char(sale_date, 'YYYY-MM') = '2022-11' ;
